879923aa3890d498b0beebe175b07dcc95237bd6
Dahamboseth <bosethwanapura@gmail.com>
2025-01-02 12:01:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jzICCaYI#EBpCzdYVYbxM_FZ2Cof73SDHSfQ6X5cFZp7MX6Tlp9w", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "nmhQDQpR#aY_m_x_tUOlVeAlSOhHxLVEX28lmzSMQlh49TzDtVDI", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
